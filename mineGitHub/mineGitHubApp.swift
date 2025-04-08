4ffb74df47a0f6b9b0a5937b859df7e3a9a4d681
Gaurav <gouravpaul7781@gmail.com>
2025-04-08 07:49:24 +0530

    Initial Commit

---
+++ mineGitHub/mineGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  mineGitHubApp.swift
+//  mineGitHub
+//
+//  Created by Gourav Paul on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct mineGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mineGitHubTests/mineGitHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  mineGitHubTests.swift
+//  mineGitHubTests
+//
+//  Created by Gourav Paul on 08/04/25.
+//
+
+import Testing
+@testable import mineGitHub
+
+struct mineGitHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
